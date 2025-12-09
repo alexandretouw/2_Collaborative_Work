84a29b3448efb8ca25b7c038864c3e5bbf872535
alextouw <108679510+alextouw@users.noreply.github.com>
2025-12-09 18:28:31 +0100

    deleted library

--- code/1_code_cleaning/code_cleaning.R
+++ code/1_code_cleaning/code_cleaning.R
@@ -5,7 +5,7 @@ library(dplyr)
 library(stringr)
 library(readr)
 library(here)
-library(tidyverse)
+
 #Data import
 members <- read_delim(here("data_in","dial_members.csv"), delim = ";")
 #Set seed 
